ed79f72cfaa9d290de7d2afd1b926c2bda925a76
Elina <tubiselina@gmail.com>
2020-10-28 00:25:15 +0000

    some functions replaced for better efficiency

--- neighbour.py
+++ neighbour.py
@@ -9,6 +9,7 @@ Created on Tue Oct 25 12:00:35 2016
 import numpy as np
 import time
 import errno
+from numba import jit
 N=np.int(input('N = '))
 seed=np.int(input('Random number seed = '))
 np.random.seed(seed)
@@ -17,24 +18,29 @@ start_time=time.time()
 # deliberately slow code to find nearest neighbours within periodic unit cube
 #
 #  You may only change the code in this cell (i.e. between here and "toc")
-#
-s=np.zeros(shape=(N,N))
-#array to store nearest neighbour
-match=np.zeros(N)
+"""@jit(nopython=True)
+def choose_vals(sq_dist, a):
+    val = np.argsort(sq_dist)[1]
+    return val"""
 
-for a in range(N):
-    #find distance between coordinates of a given point and all other points
-    distance = np.abs(pos - pos[:, a].reshape(3, 1))
-    #take the fact that space loops around into account
-    distance = np.where(distance < 0.5, distance, distance - 1)
-    #find total ditance squared with pythagoras and sort the array
-    #first entery is 0 because it's distance to the point itself
-    sq_dist = np.sum(np.power(distance, 2), axis=0)
-    #sort array with distances to get corresponding indecies and store smallest distance
-    match[a] = np.argsort(sq_dist)[1]
+def find_nearest_neghbour(N):
+    #array to store nearest neighbours
+    match=np.zeros(N)
 
+    for a in range(N):
+        #find distance between coordinates of a given point and all other points
+        distance = np.abs(pos - pos[:, a].reshape(3, 1))
+        #take the fact that space loops around into account
+        distance = np.where(distance < 0.5, distance, distance - 1)
+        #find total ditance squared with pythagoras and sort the array
+        #sort array with distances to get corresponding indecies and store smallest distance
+        match[a] = np.argsort(np.add.reduce(distance**2, axis=0))[1]
 
+    return match
+
+match = find_nearest_neghbour(N)
 #print(match)
+
 end_time=time.time() #toc
 print('Elapsed time = ',repr(end_time-start_time))
 
